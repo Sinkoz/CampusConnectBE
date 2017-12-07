a4f146dedd1d8ed7d30913883d24ff250762e87d
Xinchen <sinkozsinkoz@gmail.com>
2017-12-06 17:34:34 -0800

    database fixed

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,22 +1,34 @@
 package controller;
 
+import Repositories.PostRepository;
+import model.Post;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    @Autowired
+    private PostRepository postRepository;
+
+    @RequestMapping("/save")
+    public String save(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        postRepository.save(new Post(1));
         return "index";
     }
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/", method= RequestMethod.GET)
     public String home(Model model) {
         return "index";
     }
 
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
 }
